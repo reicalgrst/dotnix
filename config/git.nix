f60b75ad6a3516b2503bfde454353919fab4a623
reicalgrst <240953095+reicalgrst@users.noreply.github.com>
2025-11-02 10:12:01 +0900

    add git alias

--- config/git.nix
+++ config/git.nix
@@ -6,6 +6,21 @@
     userName = "reicalgrst";
     userEmail = "240953095+reicalgrst@users.noreply.github.com";
 
+    aliases = {
+      a = "add";
+      c = "commit";
+      cl = "clone";
+      cm = "commit -m";
+      co = "checkout";
+      d = "diff";
+      f = "fetch";
+      pl = "pull";
+      ps = "push";
+      rb = "rebase";
+      rbi = "rebase -i";
+      st = "status";
+    };
+
     extraConfig = {
       init.defaultBranch = "main";
     };
